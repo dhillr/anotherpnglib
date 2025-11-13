e262efdf7c9ca137c74cdc9bcea2c58223bb4b18
dhillr <dhillonrouzeau@gmail.com>
2025-11-13 16:59:29 -0500

    .

--- include/deflate_inflate_crc.h
+++ include/deflate_inflate_crc.h
@@ -8,29 +8,15 @@ typedef struct {
 } compression_out;
 
 compression_out lz77(char* data, unsigned int len) {
-    char chars[256];
-    int indices[256];
+    char* sliding_window = malloc(1);
 
     int chars_len = 0;
 
     for (int i = 0; i < len; i++) {
-        bool is_new_char = true;
-        for (int j = 0; j < chars_len; j++) {
-            if (data[i] == chars[j]) {
-                is_new_char = false;
-                break;
-            }
-        }
-
-        if (is_new_char) {
-            chars[chars_len] = data[i];
-            indices[chars_len] = i; 
-            chars_len++;
-        }
+        
     }
 
-    for (int i = 0; i < chars_len; i++)
-        printf("%c - %d\n", chars[i], indices[i]);
+    free(sliding_window);
 
     return (compression_out){NULL, 0};
 }
